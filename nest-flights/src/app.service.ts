a8733cfb55dd4aeaa911d13b3c9087ef73cb554a
PSkogs22 <paul.skogen2@gmail.com>
2020-12-08 09:47:12 -0600

    testing out Git remote tracking after cleanup

--- nest-flights/src/app.service.ts
+++ nest-flights/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World of Git!';
+    return 'Hello World of GitHub!';
   }
 }
